8ac464ce3c76cbdc40cc780d0d84b2a00ebbee24
Seongjae <chiron8963@gmail.com>
2020-11-12 00:06:41 +0900

    04-4 movie-clone prop-type

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Movie from './Movie';
+import PropTypes from 'prop-types';
 
-function Food({name, picture}){
+function Food({name, picture, rating}){
   return (
     <div>
       <h1>I like {name}</h1>
+      <h4>{rating}/5.0</h4>
       <img src={picture} alt={name}/>
     </div>
   )
@@ -15,21 +16,25 @@ const foodLike = [
     id:1,
     name: 'Kimchi',
     image: 'https://www.maangchi.com/wp-content/uploads/2019/11/vegetarian-kimchi.jpg',
+    rating: 5,
   },
   {
     id:2,
     name: 'Samgyeopsal',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Samgyeopsal-gui.jpg/1200px-Samgyeopsal-gui.jpg',
+    rating: 4,
   },
   {
     id:3,
     name: 'Bibimbap',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/1200px-Dolsot-bibimbap.jpg',
+    rating: 3.5,
   },
   {
     id:4,
     name: 'Doncasu',
     image: 'http://seonkyounglongest.com/wp-content/uploads/2017/07/donkasu-02-1000x667.jpg?x61413',
+    rating: 2,
   },
 ];
 
@@ -41,10 +46,16 @@ function App() {
   return (
     <div>
      {foodLike.map(dish => (
-       <Food key={dish.id} name={dish.name} picture={dish.image}/>
+       <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
      ))}
     </div>   
   );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
 }
 
 export default App;
